000f2e9a1abad03964e2179248105f610fae7505
Žygimantas Pleskevičius <38924118+ZygisP@users.noreply.github.com>
2018-12-27 18:28:37 +0200

    Add files via upload

--- dom.js
+++ dom.js
@@ -68,7 +68,7 @@ var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.color = 'coral'; */
 
 // QUERY SELECTORALL
-var titles = document.querySelectorAll('.title');
+/* var titles = document.querySelectorAll('.title');
 console.log(titles);
 
 titles[0].textContent = 'Hello';
@@ -79,4 +79,82 @@ var even = document.querySelectorAll('li:nth-child(even)');
 for (var i = 0; i < odd.length; i++){
     odd[i].style.backgroundColor = '#f4f4f4';
     even[i].style.backgroundColor = '#ccc';
-}
+} */
+
+// TRAVERSING THE DOM
+var itemList = document.querySelector('#items');
+// parentNode
+/* console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode); */
+
+// parentElement
+/* console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement); */
+
+// childNodes useless
+// console.log(itemList.childNodes);
+
+// children
+/* console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow'; */
+
+// firstChild useless
+// console.log(itemList.firstChild);
+
+// firstElementChild
+/* console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1'; */
+
+
+// lastChild useless
+// console.log(itemList.lastChild);
+
+// lastElementChild
+/* console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4'; */
+
+// nextSibling useless
+// console.log(itemList.nextSibling);
+
+// nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+
+// previousElementSibling
+/* console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green'; */
+
+// createElement
+
+// Create a div
+var newDiv = document.createElement('div');
+
+// add a class
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '3rem';
+
+container.insertBefore(newDiv, h1);
+
